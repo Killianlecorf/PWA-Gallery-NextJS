15976b9ae04691aed178c96b63b844730ca7e19a
killian-lecorf <killianlecorf@outlook.fr>
2024-07-15 12:26:09 +0200

    add button design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,44 @@
-"use client";
-import useAuth from '../hooks/useAuth';
+'use client';
+import Cookies from 'js-cookie';
+import Link from 'next/link';
+import './index.scss'
 
-export default function Register() {
+export default function Home() {
 
-  useAuth()
+  const token = Cookies.get('token');
+  
+  const displayToken = () => {
+    if (!token) {
+      return (
+        <div className='displayConnexionButton'>
+          <Link href='/login'>
+            <button>Login</button>
+          </Link>
+          <Link href='/register'>
+            <button>Register</button>
+          </Link>
+        </div>
+      )
+    }
+    return (
+        <div className='displayConnexionButton'>
+            <button>Delete user</button>
+        </div>
+    );
+  }
 
   return (
-    <main>
-      <div>
-        <p>page</p>
+    <main className='mainPage'>
+      {displayToken()}
+      <h1>Galery Picture</h1>
+    <div className="allGalery">
+      <div className="privateGalery">
+        <h2>Private galery</h2>
       </div>
+      <div className="publicGalery">
+        <h2>Public galery</h2>
+      </div>
+    </div>
     </main>
   );
 }
