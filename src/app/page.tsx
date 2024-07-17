b2fe2c762bbedd34cb0266646cbd9e7fadc8d2c5
killian-lecorf <killianlecorf@outlook.fr>
2024-07-17 12:33:56 +0200

    Fix: edit classname connexion button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,29 @@
 import Cookies from 'js-cookie';
 import Link from 'next/link';
 import './index.scss'
+import { useState } from 'react';
+import FileDropzone from '@/Components/fileDropzone/FIleDropzone';
 
 export default function Home() {
+    const [files, setFiles] = useState<File[]>([]);
 
   const token = Cookies.get('token');
   
-  const displayToken = () => {
+  const displayButtonConnexion = () => {
     if (!token) {
       return (
         <div className='displayConnexionButton'>
           <Link href='/login'>
-            <button>Login</button>
+            <button className='StyleButton'>Login</button>
           </Link>
           <Link href='/register'>
-            <button>Register</button>
+            <button className='StyleButton'>Register</button>
           </Link>
         </div>
       )
     }
+
+
     return (
         <div className='displayConnexionButton'>
             <button>Delete user</button>
@@ -27,18 +32,43 @@ export default function Home() {
     );
   }
 
+  const handleDrop = (acceptedFiles: File[]) => {
+    setFiles(acceptedFiles);
+  };
+
+  const displayDropzone = () => {
+    if (!token) {
+      return (
+        <div className='dragDropContent'>
+        <h1>Système de Drag & Drop de Fichiers</h1>
+        <FileDropzone onDrop={handleDrop} />
+            <div style={{ marginTop: '20px' }}>
+                <h2>Fichiers téléchargés :</h2>
+                <ul>
+                {files.map((file, index) => (
+                    <li key={index}>{file.name}</li>
+                ))}
+                </ul>
+            </div>
+            {!files ? '' : <button className='StyleButton'>Envoyer</button>}
+        </div>
+      )
+    }
+}
+
   return (
     <main className='mainPage'>
-      {displayToken()}
-      <h1>Galery Picture</h1>
-    <div className="allGalery">
-      <div className="privateGalery">
-        <h2>Private galery</h2>
-      </div>
-      <div className="publicGalery">
-        <h2>Public galery</h2>
-      </div>
-    </div>
+        {displayButtonConnexion()}
+        <h1>Galery Picture</h1>
+        <div className="allGalery">
+            {displayDropzone()}
+            <div className="privateGalery">
+                <h2>Private galery</h2>
+            </div>
+            <div className="publicGalery">
+                <h2>Public galery</h2>
+            </div>
+        </div>
     </main>
   );
 }
