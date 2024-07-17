c14adf1fbcf68bcbae9a5dfb3bd71d80acd4b8e8
killian-lecorf <Klecorf@normandiewebschool.fr>
2024-07-17 20:15:27 +0200

    Feat: detected if auth

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,17 +1,35 @@
-"use client";
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import Cookies from 'js-cookie';
+import fetchApi from '@/utils/fetchApi';
+import { useState, useEffect } from 'react';
 
 const useAuth = () => {
-  const router = useRouter();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const token = Cookies.get('token');
-    if (!token) {
-      router.push('/login');
-    }
-  }, [router]);
+    const checkAuth = async () => {
+      try {
+        const response = await fetchApi('/user/isAuth', 'GET');
+
+        if (response.ok) {
+          const data = response.data;
+
+          setIsAuthenticated(data.isAuthenticated);
+        } else {
+          console.error('Erreur de réponse:', response.message);
+          setIsAuthenticated(false);
+        }
+      } catch (error) {
+        console.error('Erreur lors de la vérification de l\'authentification:', error);
+        setIsAuthenticated(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  return { isAuthenticated, loading };
 };
 
 export default useAuth;
